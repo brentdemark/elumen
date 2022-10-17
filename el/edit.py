9cf09f89c5f68e1c0262a4664de744c301efb848
brentdemark <brent.demark@gmail.com>
2022-10-16 20:28:12 -0500

    Add some more documentation.

--- el/edit.py
+++ el/edit.py
@@ -15,17 +15,36 @@ class FileBuffer:
     def __init__(self):
         self.content = None
 
-    def get_text_view(self, start_line, end_line, start_column, end_column) -> str:
+    def get_text_view(self, start_line: int, end_line: int, start_column: int, end_column: int) -> str:
+        """
+        Get a portion of the buffer to view
+        :param start_line: first line of the desired view
+        :param end_line: last line of the desired view
+        :param start_column: first column of the desired view
+        :param end_column: last column of the desired view
+        :return: String from the buffer content within the given parameters
+        """
         rows = self.content.split(os.linesep)[start_line:end_line]
         trimmed_rows = [row[start_column:end_column] for row in rows]
         return os.linesep.join(trimmed_rows)
 
-    def insert_char_at(self, char: str, line, column):
+    def insert_char_at(self, char: str, line: int, column: int):
+        """
+        Insert a character into the content at the give line and column
+        :param char: Character to insert
+        :param line: Line to insert at
+        :param column: Column to insert at
+        """
         rows = self.content.split(os.linesep)
         rows[line] = rows[line][:column] + char + rows[line][column:]
         self.content = os.linesep.join(rows)
 
     def delete_char_at(self, line: int, column: int):
+        """
+        Delete a character at the given line and column
+        :param line: Line number to delete at
+        :param column: Column number to delete at
+        """
         rows = [x + os.linesep for x in self.content.split(os.linesep)]
         if rows[-1] == os.linesep:
             rows = rows[:-1]
@@ -46,6 +65,11 @@ class TextEditor:
         self.text_column_start = 0
 
     def load_file_to_buffer(self, file_name: str):
+        """
+        load the given file into the buffer, if it exists,
+        otherwise initialize the buffer to an empty string.
+        :param file_name:
+        """
         file_path = Path(file_name)
         text = ''
         if file_path.is_file():
@@ -54,11 +78,19 @@ class TextEditor:
         self.buffer.content = str(text)
 
     def save_buffer_to_file(self, file_name: str):
+        """
+        Save the buffer to the given file
+        :param file_name:
+        """
         file_path = Path(file_name)
         with open(file_path, 'w') as file:
             file.writelines(self.buffer.content)
 
     def edit(self, file_name: str):
+        """
+        Present the given file for editing within the terminal
+        :param file_name: The file to edit
+        """
         self.stdscr.keypad(True)
         self.stdscr.clear()
         self.load_file_to_buffer(file_name)
@@ -78,30 +110,47 @@ class TextEditor:
             self.save_buffer_to_file(file_name)
 
     def show_buffer_content(self):
+        """
+        Get a portion of the buffer to show and display it in the terminal
+        """
         content = self.buffer.get_text_view(self.text_line_start, self.text_line_end(), self.text_column_start,
                                             self.text_column_end())
         self.stdscr.addstr(0, 0, content.encode('utf_8'))
 
     def text_line_end(self):
+        """
+        Get the last line of the content to view base on the start and window size
+        """
         return self.text_line_start + self.term_height - 1
 
     def text_column_end(self):
+        """
+        Get the last column of the content to view base on the start and window size
+        """
         return self.text_line_start + self.term_width - 1
 
     def can_scroll_down(self, y):
+        """
+        :param y: The current cursor y position
+        :return: True if the cursor is at the bottom and there is more text below to show
+        """
         return y == self.term_height - 2 and self.text_line_end() < len(self.buffer.content.split(os.linesep))
 
     def can_scroll_up(self, y):
+        """
+        :param y: The current cursor y position
+        :return: True if the cursor is at the top and there is more text above to show
+        """
         return y == 0 and self.text_line_start
 
     def handle_keyboard_input(self, x, y, key):
-        '''
+        """
         Handle keyboard input
         :param x: Current cursor x position
         :param y: Current cursor y position
         :param key: The key that was pressed ASCII value
         :return: new cursor x, y and message
-        '''
+        """
         message = ''
         if key == curses.KEY_LEFT and x > 0:
             x -= 1
